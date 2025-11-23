4dd71491136988e5ba4a6b96f50d0dd8f02daaf8
Erik Albecker <erik.albecker@sap.com>
2025-11-23 23:11:42 +0100

    setup websocket receiver

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+#!/usr/bin/python
+
+import websocket
+import rel
+
+def on_message(ws, message):
+    print()
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+def on_open(ws):
+    print("### opened ###")
+    ws.send('{"a":111}')
+
+
 def main():
-    pass
+
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://ws1.blitzortung.org/",
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.run_forever(dispatcher=rel,
+                   reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
+
+
 
 if __name__ == '__main__':
     main()
